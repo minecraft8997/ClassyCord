1fbb980ac23014d9ffe224e9a73c0238f2003aa7
minecraft8997 <35162137+minecraft8997@users.noreply.github.com>
2024-01-15 23:29:42 +0300

    Increment version

--- src/ru/deewend/classycord/ClassyCord.java
+++ src/ru/deewend/classycord/ClassyCord.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 import java.util.*;
 
 public class ClassyCord {
-    public static final String VERSION = "0.9.2";
+    public static final String VERSION = "0.9.3";
     public static final boolean DEBUG = Boolean
             .parseBoolean(System.getProperty("ccdebug", "false"));
 
